838b9060e62de880cc66baa5f93ff80f3ade3759
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-02-22 20:48:00 +0900

    first commit

---
+++ GitPractice/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GitPractice
+//
+//  Created by 坂本龍哉 on 2021/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var name = "坂本"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
+
